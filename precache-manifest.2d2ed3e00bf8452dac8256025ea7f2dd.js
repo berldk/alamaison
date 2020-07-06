60403c568618895399c2981a9b7ca7a01867dbf1
COOVI DOSSOU-KOKO <bdossoukoko@gmail.com>
2020-07-06 00:32:42 -0400

    Updates

---
+++ precache-manifest.2d2ed3e00bf8452dac8256025ea7f2dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b9146d9c265151df26a1654338ef4b",
+    "url": "/alamaison/index.html"
+  },
+  {
+    "revision": "0536af28cb46750c9315",
+    "url": "/alamaison/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/alamaison/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alamaison/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0536af28cb46750c9315",
+    "url": "/alamaison/static/js/main.4c9223cd.chunk.js"
+  },
+  {
+    "revision": "6da23b1649424bb10cc0",
+    "url": "/alamaison/static/js/runtime-main.51c08830.js"
+  }
+]);
